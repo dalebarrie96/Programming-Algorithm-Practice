e3cfc7dc8ae5a7855cbf146f1c375da48c5283f3
Dale Barrie <dalebarrieit@gmail.com>
2021-02-18 14:04:28 +0000

    Project Setup and First beginner algorithm

---
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/WayTooLongWords.java
@@ -0,0 +1,22 @@
+package dbarrie.codeforces.beginner;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class WayTooLongWords {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		
+		int n = Integer.valueOf(str);
+		
+		IntStream.range(0, n).forEach(i -> {
+			String word = sc.nextLine();
+			System.out.println(word.length() > 10 ? "" + word.charAt(0) + (word.length()-2) + word.charAt(word.length()-1) : word);
+		});
+		
+		
+	}
+
+}
