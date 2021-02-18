167ad78c4c081f2d0af11b8ede79970e338f4f7b
Dale Barrie <dalebarrieit@gmail.com>
2021-02-18 17:27:54 +0000

    adding comments and url to java class

--- Competative Programming - Practice/src/dbarrie/codeforces/beginner/WayTooLongWords.java
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/WayTooLongWords.java
@@ -3,6 +3,11 @@ package dbarrie.codeforces.beginner;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
+/*
+ * Codeforces WayTooLongWords - Difficulty 800
+ * 
+ * https://codeforces.com/problemset/problem/71/A
+ */
 public class WayTooLongWords {
 
 	public static void main(String[] args) {
