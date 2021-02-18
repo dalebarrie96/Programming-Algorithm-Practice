3688007cf830306b02d993f671d68e7227c04ed3
Dale Barrie <dalebarrieit@gmail.com>
2021-02-18 17:43:51 +0000

    Adding comments to watermelon

--- Competative Programming - Practice/src/dbarrie/codeforces/beginner/Watermelon.java
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/Watermelon.java
@@ -3,6 +3,11 @@ package dbarrie.codeforces.beginner;
 import java.util.Scanner;
 import java.util.stream.IntStream;
 
+/*
+ * Codeforces Watermelon - Difficulty 800
+ * 
+ * https://codeforces.com/problemset/problem/4/A
+ */
 public class Watermelon {
 
 	public static void main(String[] args) {
