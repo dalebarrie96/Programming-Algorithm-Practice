1e40159aa939584458e4ba8d61c42c6bd659cf17
Dale Barrie <dalebarrieit@gmail.com>
2021-02-19 18:08:42 +0000

    Another beginner code forces solution, No java 8 this time

---
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/DominoPiling.java
@@ -0,0 +1,19 @@
+package dbarrie.codeforces.beginner;
+
+import java.util.Scanner;
+
+/*
+ * Codeforces A Team - Difficulty 800
+ * 
+ * https://codeforces.com/problemset/problem/50/A
+ */
+public class DominoPiling {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		System.out.println((x * y) / 2);
+	}
+}
