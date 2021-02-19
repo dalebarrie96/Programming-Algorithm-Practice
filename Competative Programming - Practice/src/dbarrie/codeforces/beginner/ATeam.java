40f800c7840ff96d02be9985d5ea1b1e7ca2139b
Dale Barrie <dalebarrieit@gmail.com>
2021-02-19 17:36:06 +0000

    Updating the solution to user Java 8 Functionality
    Again this is slightly less performant but i want to get more experience
    with java 8

--- Competative Programming - Practice/src/dbarrie/codeforces/beginner/ATeam.java
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/ATeam.java
@@ -1,26 +1,29 @@
 package dbarrie.codeforces.beginner;
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
 
+/*
+ * Codeforces A Team - Difficulty 800
+ * 
+ * https://codeforces.com/problemset/problem/231/A
+ */
 public class ATeam {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.valueOf(sc.nextLine());
 		
-		int count = 0;
-		
-		for(int i = 0; i < n; i++) {
+		IntPredicate predicate = ( i ->{
 			String canResolveMap = sc.nextLine();
 			
 			canResolveMap = canResolveMap.replace(" ", "").replace("0", "");
 			
-			if(canResolveMap.length() >= 2) count++;
-			
-		}
+			return canResolveMap.length() >= 2;
+		});
 		
-		System.out.println(count);
+		System.out.println(IntStream.range(0, n).filter(predicate).count());
 
 	}
-
 }
