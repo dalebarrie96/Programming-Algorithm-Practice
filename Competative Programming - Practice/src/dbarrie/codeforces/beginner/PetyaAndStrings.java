1566c90faa7f8de09006ae54c79356d5197bdb12
Dale Barrie <dalebarrieit@gmail.com>
2021-02-25 00:03:36 +0000

    Small refactor and comments added to code, I could use the compareTo method as this compares lexicographically but i saw this from another solution so don't want to steal it

--- Competative Programming - Practice/src/dbarrie/codeforces/beginner/PetyaAndStrings.java
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/PetyaAndStrings.java
@@ -2,6 +2,14 @@ package dbarrie.codeforces.beginner;
 
 import java.util.Scanner;
 
+/*
+ * Codeforces Petya and Strings - Difficulty 800
+ * 
+ * This problem was to compare 2 strings together lexicographically,
+ * Iv since learned that this is exactly what the "compareTo" method on strings does
+ * 
+ * https://codeforces.com/problemset/problem/112/A
+ */
 public class PetyaAndStrings {
 
 	public static void main(String[] args) {
@@ -10,11 +18,11 @@ public class PetyaAndStrings {
 		String str2 = sc.nextLine().toLowerCase();
 		
 		String res = "0";
-	
+		
 		for(int i = 0; i < str1.length(); i++) {
 			if(str1.charAt(i) > str2.charAt(i)) res = "1";
 			if(str2.charAt(i) > str1.charAt(i)) res = "-1";
-			if(res != "0") break;
+			if(!res.equals("0")) break;
 		}
 		
 		System.out.println(res);
