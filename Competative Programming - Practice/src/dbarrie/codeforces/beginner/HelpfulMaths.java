a53eaa84184b2fa8e2d8eca585a4741dba30666f
Dale Barrie <dalebarrieit@gmail.com>
2021-02-28 20:43:47 +0000

    Refctoring and tidying up helpfulmaths

--- Competative Programming - Practice/src/dbarrie/codeforces/beginner/HelpfulMaths.java
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/HelpfulMaths.java
@@ -3,21 +3,18 @@ package dbarrie.codeforces.beginner;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/*
+ * Codeforces HelpfulMaths - Difficulty 800
+ * 
+ * https://codeforces.com/problemset/problem/266/A
+ */
 public class HelpfulMaths {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		char[] in = sc.nextLine().replaceAll("\\+", "").toCharArray();
+		String[] in = sc.nextLine().split("\\+");
 		Arrays.sort(in);
 		
-		String res = "";
-		for(int i = 0; i < in.length; i++) {
-			
-			res += in[i];
-			
-			if(in.length -1 != i) res += "+";
-		}
-		
-		System.out.println(res);
+		System.out.println(String.join("+", in));
 	}
 }
