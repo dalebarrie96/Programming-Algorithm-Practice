4d602993f58f61ac332d1af1d76799a3b58a66a3
Dale Barrie <dalebarrieit@gmail.com>
2021-03-05 23:30:18 +0000

    Code forces contains solution

---
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/Football.java
@@ -0,0 +1,18 @@
+package dbarrie.codeforces.beginner;
+
+import java.util.Scanner;
+
+/*
+ * Codeforces Football - Difficulty 900
+ * 
+ * https://codeforces.com/problemset/problem/96/A
+ */
+public final class Football {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String in = sc.nextLine();
+		
+		System.out.println(in.contains("1111111") || in.contains("0000000") ? "YES" : "NO" );
+	}
+}
