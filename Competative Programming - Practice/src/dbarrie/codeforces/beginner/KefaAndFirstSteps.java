f619759470c1ef319f179f92a9198ac66dfc09c7
Dale Barrie <dalebarrieit@gmail.com>
2021-03-11 22:00:45 +0000

    Accepted Kefa and First Steps, using DP to solve as i create the array

--- Competative Programming - Practice/src/dbarrie/codeforces/beginner/KefaAndFirstSteps.java
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/KefaAndFirstSteps.java
@@ -12,30 +12,26 @@ public class KefaAndFirstSteps {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
+		
 		int[] array = new int[n];
+		array[0] = sc.nextInt();
 		int res=0;
 		
-		for(int i=0; i < n; i++) {
-			array[i] = sc.nextInt();
-		}
+		int count=1;
 		
-		for(int i =0; i < n; i++) {
-			int count = 0;
-			int last = array[i];
+		for(int i=1; i < n; i++) {
+			array[i] = sc.nextInt();
 			
-			for(int j=i;j < n;j++) {
-				if(last <= array[j]) {
-					count++;
-				}else {
-					break;
-				}
-				last = array[j];
+			if(array[i-1] <= array[i]) {
+				count++;
+			}else {
+				if(count > res) res = count;
+				count = 1;
 			}
 			
-			if(count > res) res = count;
 		}
 		
-		System.out.println(res);
+		System.out.println(res > count ? res : count);
 		
 	}
 
