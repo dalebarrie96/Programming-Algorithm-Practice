67c3e79ae73a51116dd7e036d7d610a68759a361
Dale Barrie <dalebarrieit@gmail.com>
2021-03-14 17:41:03 +0000

    Codeforces, first attempt at Alexey and train prpblem, Wrong answer on second test

---
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/AlexeyAndTrain.java
@@ -0,0 +1,63 @@
+package dbarrie.codeforces.beginner;
+
+import java.util.Scanner;
+
+public class AlexeyAndTrain {
+
+	/*
+	 * Codeforces Alexey And Train - Difficulty 800
+	 * 
+	 * https://codeforces.com/problemset/problem/1501/A
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		int[] res = new int[t];
+		
+		for(int tc=0; tc < t; tc++) { //tc == test case
+			int s = sc.nextInt(); //s == stations
+			int[][] stationsArr = new int[s][2];
+			for(int st = 0; st < s; st++) { //st == station
+				stationsArr[st][0] = sc.nextInt();
+				stationsArr[st][1] = sc.nextInt();
+				sc.nextLine();
+			}
+			
+			String[] delays = sc.nextLine().split(" ");
+			
+			for(int d=0; d < delays.length-1; d++) { //skip last -- last == solution
+				int plannedArrival = stationsArr[d][0];
+				int actualArrival = stationsArr[d][0] + Integer.valueOf(delays[d]);
+				int plannedDepart = stationsArr[d][1];
+				int minDuration = Math.round((float)(plannedDepart - plannedArrival) / 2);
+				int travelTime = stationsArr[d+1][0] - plannedDepart;
+				
+//				System.out.println("******* Solving *******");
+//				System.out.println(stationsArr[d][0] + " - " + stationsArr[d][1]);
+//				System.out.println("**********************");
+				
+				stationsArr[d][0] = actualArrival;
+				
+				if(actualArrival > plannedDepart || plannedDepart - actualArrival < minDuration) {
+					
+					int actualDepart = minDuration <= 0 ? actualArrival + 1 : actualArrival + minDuration;
+					
+					stationsArr[d][1] = actualDepart;
+					stationsArr[d+1][0] = actualDepart + travelTime;
+				}
+				
+//				System.out.println("******* Resolved to *******");
+//				System.out.println(stationsArr[d][0] + " - " + stationsArr[d][1]);
+//				System.out.println("***************************");
+				
+				
+			}
+			
+			res[tc] = stationsArr[delays.length-1][0] + Integer.valueOf(delays[delays.length-1]);
+		}
+		
+		for (int i : res) {
+			System.out.println(i);
+		}
+	}
+}
---
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/FairDivision.java
@@ -0,0 +1,33 @@
+package dbarrie.codeforces.beginner;
+
+import java.util.Scanner;
+
+/*
+ * Codeforces Fair Division - Difficulty 800
+ * 
+ * https://codeforces.com/problemset/problem/1472/B
+ */
+public class FairDivision {
+	
+	// TODO: Finish this please.
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int no = sc.nextInt();
+		String[] res = new String[no];
+		
+		for(int i=0; i < no; i++) {
+			int sweets = sc.nextInt();
+
+			for(int s=0; s < sweets; s++) {
+				int grams = sc.nextInt();
+			}
+			
+			
+		}
+		
+		for(int i=0; i < res.length;i++) {
+			System.out.println(res[i]);
+		}
+	}
+
+}
