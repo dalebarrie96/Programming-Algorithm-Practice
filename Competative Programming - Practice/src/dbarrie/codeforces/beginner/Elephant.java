57d310c5d8fb13bab4ee5109f0b07f52669187b7
Dale Barrie <dalebarrieit@gmail.com>
2021-03-23 21:27:15 +0000

    Refactored elephent problem down to a single line

--- Competative Programming - Practice/src/dbarrie/codeforces/beginner/Elephant.java
+++ Competative Programming - Practice/src/dbarrie/codeforces/beginner/Elephant.java
@@ -12,29 +12,9 @@ public class Elephant {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int count = 0;
+		int res = n%5 == 0 ? n/5 : n/5 + 1;
 		
-		do {
-			count++;
-			if(n >=5) {
-				n = n - 5;
-			}
-			else if(n == 4) {
-				n = n - 4;
-			}
-			else if(n == 3) {
-				n = n - 3;
-			}
-			else if(n == 2) {
-				n = n - 2;
-			}
-			else if(n == 1) {
-				n = n - 1;
-			}
-			
-		}while(n > 0);
-		
-		System.out.println(count);
+		System.out.println(res);
 
 	}
 
